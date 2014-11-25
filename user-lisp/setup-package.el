dee660b2ce9433dced6afa96020ec776412ecc45
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2014-11-25 17:03:29 -0500

    Update MELPA address

--- user-lisp/setup-package.el
+++ user-lisp/setup-package.el
@@ -3,7 +3,7 @@
 ;; Add repos
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")
+        ("melpa" . "http://melpa.org/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
